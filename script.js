c51f0ca93352139dab0dfb0ec4de9cb109b50446
Teambxray <teambrandxray@gmail.com>
2025-02-15 14:23:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 gsap.to(".scan-line", {
-    y: "100vh", // Move from top to bottom
-    duration: 3, // 3 seconds for a full pass
+    y: "100vh", // Moves from top to bottom
+    duration: 3, // Duration of movement
     ease: "power2.inOut",
     repeat: -1, // Infinite loop
-    yoyo: true // Moves up and down continuously
+    yoyo: true, // Moves up and down continuously
 });
